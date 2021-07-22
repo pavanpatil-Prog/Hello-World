b94291956ed527cebdd96d390fe98d23e6a86535
Pavan Patil <70448117+pavanpatil-Prog@users.noreply.github.com>
2021-07-22 14:48:23 +0530

    updated app.py for releive mail

--- HelloWorld_MyCloude/app.py
+++ HelloWorld_MyCloude/app.py
@@ -1,7 +1,6 @@
-import logging
-
 from flask import Flask
 from flask import json
+import logging
 
 app = Flask(__name__)
 
@@ -19,7 +18,7 @@ def healthcheck():
 @app.route('/metrics')
 def metrics():
     response = app.response_class(
-            response=json.dumps({"status":"success", "code":0, "data":{"UserCount":140,"UserCountActive":23}}),
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
             status=200,
             mimetype='application/json'
     )
@@ -30,11 +29,23 @@ def metrics():
 @app.route("/")
 def hello():
     app.logger.info('Main request successfull')
-
-    return "Hello World!"
+    Message= "New Beginings , What..!!! Why..? and How? <br> Hi All, <br> its my last working day here, \
+    <br> So I would like to take this opportunity to back in time and recall my story\
+    <br> <li> By the time I finished gratuation it was too late to decide WHAT..? Next  \
+    <br> <li>Just when I get answer of that, HR asked another Question  WHY..? this\
+    <br> It took me 5 years to find the answer of that question\
+    <br> <br>So I would like to thank The leader who introduced me in ZF to another the leader \
+    <br> who gave me opportunity in ZF ITC to explore new world\
+    <br>Most important learning in ZF for me is that I learned Not only to have an Opinion, but to belive it and wear it with confidance\
+    <br> <li>Rest became easy beacuse all of you guys who helped me in my daily work when I was finding answers to different WHY..?\
+    <br> <li>Thank you very much to all and All the very best for future\
+    <br><br><br> <li>This exit is however special as my 8 years streak in Hyderabad is ending\
+    <br> Gonna Miss Biryani and Trivikrams One liners a lot\
+    <br> <br><br><br>Best Regards, <br>Pavan Patil"
+    return Message
 
 if __name__ == "__main__":
     ## stream logs to a file
     logging.basicConfig(filename='app.log',level=logging.DEBUG)
     
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0')
